b35b5a48a96d2e07005c10f37cca660f41722767
Joseph <josephtecson18@gmail.com>
2021-12-03 00:44:08 -0800

    Section 3.11

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
         <footer>
-            footer
+            <Container>
+                <Row>
+                    <Col className='text-center py-3'>Copyright &copy; ProShop</Col>
+                </Row>
+            </Container>
         </footer>
     )
 }
