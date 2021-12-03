81789a255e77ed93f2a3ab4a0e280e479e9ea542
Joseph <josephtecson18@gmail.com>
2021-12-03 01:35:57 -0800

    Section 3.12

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios'
+
 
 const ProductScreen = () => {
     const match = useParams();
-    const product = products.find((p) => p._id === match.id)
+    // const product = products.find((p) => p._id === match.id)
+
+    const [product, setProduct] = useState({})
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${match.id}`)
+            setProduct(data)
+        }
+
+        fetchProduct()
+    }, [match])
 
     return <div>
         <Link className='btn btn-light my-3' to='/'>
